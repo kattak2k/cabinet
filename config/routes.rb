60b10d859f60ec2a579eeef634ec8a3ee18081f6
kattak2k <kkiran.ror@gmail.com>
2017-03-11 02:36:41 +0530

    add SCSS and normalize and stylesheets

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   #get 'welcome/index'
 
+resources :docs
+
+#For the users who login => show index of Docs
+authenticated :user do
+  root "docs#index", as: "authenticated_root"
+end
+
+# For the user who do not login => show index of welcome page
 root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-resources :docs
 end
